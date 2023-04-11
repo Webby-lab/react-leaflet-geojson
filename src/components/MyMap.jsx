450ac03ef2c59578e540ed50c79675e8cdba98ed
Webby-lab <keviczkyne.kover.krisztina@gmail.com>
2023-04-11 16:02:11 +0200

    map ang geojson layer added

--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { MapContainer, GeoJSON, TileLayer } from "react-leaflet";
+
+import mapdata from './../data/countries.json'
+import "leaflet/dist/leaflet.css";
 
 class MyMap extends Component {
     state = {}
@@ -6,6 +10,15 @@ class MyMap extends Component {
         return (
             <div>
                 <h2 style={{ textAlign: "center" }}>My map with GeoJSON objects</h2>
+
+                <MapContainer style={{ height: "100vh" }} zoom={4} center={[20, 100]}>
+
+                    <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+                    />
+                    <GeoJSON data={mapdata.features}/>
+                </MapContainer>
             </div>
         );
     }
