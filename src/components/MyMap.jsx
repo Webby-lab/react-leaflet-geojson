d25c76d37c11fcb290aace5d5aab2be14a9e0e30
Webby-lab <keviczkyne.kover.krisztina@gmail.com>
2023-04-11 15:44:41 +0200

    json file and mymap file added

---
+++ src/components/MyMap.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MyMap extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                <h2 style={{ textAlign: "center" }}>My map with GeoJSON objects</h2>
+            </div>
+        );
+    }
+}
+
+export default MyMap;
