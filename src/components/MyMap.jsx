160921e2e3db6a764024674d9c2317aa419a008f
Webby-lab <keviczkyne.kover.krisztina@gmail.com>
2023-04-11 17:02:42 +0200

    finalchanges

--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { MapContainer, GeoJSON, TileLayer } from "react-leaflet";
-
+import { Marker, Popup } from 'react-leaflet';
 import mapdata from './../data/countries.json'
 import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
+L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.5.0/dist/images/";
 
 class MyMap extends Component {
-    state = {}
+    state = {
+        //Budapest
+        lat:47.4983,
+        lng:19.0408,
+        zoom: 4
+    };
 
     countryStyle = {
         fillColor : "red",
@@ -42,17 +49,22 @@ class MyMap extends Component {
 
 
     render() {
+        const center = [this.state.lat, this.state.lng];
         return (
             <div>
                 <h2 style={{ textAlign: "center" }}>My map with GeoJSON objects</h2>
 
-                <MapContainer style={{ height: "100vh" }} zoom={4} center={[20, 100]}>
+                <MapContainer style={{ height: "100vh" }} zoom={this.state.zoom} center={center}>
 
                     <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
                     <GeoJSON style={this.countryStyle} data={mapdata.features} onEachFeature={this.onEachCountry}/>
+
+                    <Marker position={center}>
+                        <Popup>Budapest &#x1F49A;</Popup>
+                    </Marker>
 
                 </MapContainer>
             </div>
